67f0733865b659c6146b4e52e7fd372870cf36dd
Daianes26 <daianeasilva26@gmail.com>
2019-07-14 16:09:23 -0400

    Restaurant project

---
+++ restaurant/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.urls import include, re_path
+from . import views
+from restaurant.views import (ContactView, AboutTemplateView, 
+restaurant_listview, RestaurantListView, RestaurantDetailtView)
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('about/', AboutTemplateView.as_view(), name='about'),
+    path('restaurant/', views.restaurant_listview, name='restaurant-list'),
+]
+
+urlpatterns += [
+    # re_path(r'^restaurant/(?P<slug>\w+)/$', RestaurantListView.as_view()),
+    re_path(r'^restaurant/(?P<rest_id>\w+)/$', RestaurantDetailtView.as_view(), name='restaurant-detail'),
+]
---
+++ restaurant/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView
+import random
+from .models import Restaurant
+from django.db.models import Q
+
+
+# Create your views here.
+
+def index(request):
+    num = None
+    some_list = [
+        random.randint(0, 1000000), 
+        random.randint(0, 1000000),
+        random.randint(0, 100000),
+    ]
+    condition_bool_item = True
+    if condition_bool_item:
+        num = random.randint(0, 100000)
+
+    context = {
+        'num': num,
+        'some_list': some_list
+
+    }
+    
+    return render(request, 'index.html', context)
+
+
+
+def restaurant_listview(request):
+    queryset = Restaurant.objects.all()
+    context = {
+        'object_list': queryset,
+    }
+    
+    return render(request,'restaurant/restaurant_list.html', context)
+
+
+class RestaurantListView(ListView):
+    def get_queryset(self):
+        slug = self.kwargs.get("slug")
+        if slug:
+            queryset=Restaurant.objects.filter(
+                Q(category__iexact=slug) |
+                Q(category__icontains=slug) 
+            )
+        else:
+            queryset=Restaurant.objects.all()
+        return queryset
+
+
+class RestaurantDetailtView(DetailView):
+    queryset=Restaurant.objects.all()
+    def get_object(self, *args, **kwargs):
+        rest_id = self.kwargs.get('rest_id')
+        obj = get_object_or_404(Restaurant, pk=rest_id) 
+        return obj
+
+
+class ContactView(View):
+    def get(self, request):
+        context = {}
+        return render(request, 'contact.html', context)
+
+class AboutTemplateView(TemplateView):
+    template_name='about.html'
+
